e4ebfef6fa10c65201f7ac6afe8f96f5191049fc
leehj01 <leehjung55@gmail.com>
2021-01-09 23:57:27 +0900

    commit

--- CodingTestClass/06. 다이나믹 프로그래밍.py
+++ CodingTestClass/06. 다이나믹 프로그래밍.py
@@ -49,6 +49,20 @@ def fibo(x):
     return d[x]
 
 print(fibo(99)) # 218922995834555169026
+
+# +
+d = [0] * 100
+
+def fibo(x):
+    print('f(' + str(x) + ')', end = ' ')
+    if x == 1 or x == 2 :
+        return 1 
+    if d[x] != 0 :
+        return d[x]
+    d[x] = fibo(x- 1) +fibo(x -2)
+    return d[x]
+
+fibo(6) # f(6) f(5) f(4) f(3) f(2) f(1) f(2) f(3) f(4)  
 # -
 
 # ### 피보나치 수열 : 보텀업 다이나믹 프로그래밍 소스 코드 
