d71f230fd15c39400601057f6c9244b0c6772c4c
leehj01 <leehjung55@gmail.com>
2021-01-14 23:24:23 +0900

    commit

--- CodingTestClass/11. 기타 알고리즘.py
+++ CodingTestClass/11. 기타 알고리즘.py
@@ -148,6 +148,81 @@ right = 4
 print(prefix_sum[right] - prefix_sum[left - 1]) # 70
 # -
 
+# ## 바이너리 인덱스 트리
+# -  데이터 업데이트가 가능한 상황에서 구간 합 ( Interval sum) 문제 
+# - 문제 :https://www.acmicpc.net/problem/2042
+# - 입력 
+#
+# 5 2 2
+#
+# 1
+#
+# 2
+#
+# 3
+#
+# 4
+#
+# 5
+#
+# 1 3 6
+#
+# 2 2 5
+#
+# 1 5 2
+#
+# 2 3 5
+
+# ### K&-K 계산 결과 
+
+n = 8
+for i in range(n +1):
+    print(i, "의 마지막 비트 :", (i & -i))
+
+# +
+# 데이터 개수(n) 변경횟수 (m) 구간 합 계산 횟수(k)
+n, m , k = map(int, input().split())
+
+# 전체 데이터의 개수는 최대 1,000,000 개
+arr = [0] * (n+1)
+tree = [0] * (n+1)
+
+# i번째 수까지의 누적합을 계산하는 함수
+def prefix_sum(i):
+    result = 0
+    while i > 0 :
+        result += tree[i]
+        # 0이 아닌 마지막 비트만큼 빼가면서 이동
+        i -= (i & -i)
+        
+    return result
+
+# i번째 수를 dif만큼 더하는 함수
+def update(i, dif):
+    while i <= n :
+        tree[i] += dif
+        i += ( i & -i )
+        
+# start 부터 end 까지의 구간 합을 계산하는 함수
+def interval_sum(start, end):
+    return prefix_sum(end) - prefix_sum(start -1)
+
+for i in range(1, n +1 ):
+    x = int(input())
+    arr[i] = x
+    update(i, x)
+    
+for i in range(m + k):
+    a ,b ,c = map(int, input().split())
+    # 업데이트 연산인 경우
+    if a == 1 :
+        update(b, c - arr[b]) # 바뀐 크기 dif 만큼 적용
+        arr[b] =c
+    # 구간 합 (interval sum) 연산인 경우
+    else :
+        print(interval_sum(b, c))
+# -
+
 # ## 순열
 
 
