2d31a09d2cdbee9ba9d64cd1115d4b94f1986409
leehj01 <leehjung55@gmail.com>
2021-01-15 00:45:02 +0900

    commit

--- CodingTestClass/05. 이진탐색 알고리즘.py
+++ CodingTestClass/05. 이진탐색 알고리즘.py
@@ -204,3 +204,150 @@ print()
 in_order(tree['A'])
 print()
 post_order(tree['A'])
+# -
+# ## 기본적인 최소공통 조상(LCA)
+
+# + active=""
+# # 백준 :https://www.acmicpc.net/problem/11438
+# # 입력
+# 15
+# 1 2
+# 1 3
+# 2 4
+# 3 7
+# 6 2
+# 3 8
+# 4 9
+# 2 5
+# 5 11
+# 7 13
+# 10 4
+# 11 15
+# 12 5
+# 14 7
+# 6
+# 6 11
+# 10 9
+# 2 6
+# 7 6
+# 8 13
+# 8 15
+
+# +
+# import sys
+# sys.setrecursionlimit(int(1e5)) # 런타임 오류를 피하기
+n = int(input())
+
+parent = [0] * (n + 1) # 부모 노드 정보
+d = [0] * (n + 1)  # 각 노드 까지의 깊이
+c = [0] * (n + 1)  # 각 노드의 깊이가 계산되었는지 여부
+graph = [[] for _ in range(n +1)] # 그래프 정보
+
+for _ in range(n -1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+    
+# 루트 노드부터 시작하여 깊이를 구하는 함수
+def dfs(x, depth):
+    c[x] = True
+    d[x] = depth
+    for y in graph[x]:
+        if c[y]: # 이미 깊이를 구했다면 넘기기
+            continue
+        parent[y] = x
+        dfs(y, depth +1)
+        
+# A와 B의 최소 공통 조상을 찾는 함수
+def lca(a,b):
+    # 먼저 깊이가 동일하도록
+    while d[a] != d[b]:
+        if d[a] > d[b]:
+            a = parent[a]
+        else:
+            b = parent[b]
+            
+    # 노드가 같아지도록
+    while a != b :
+        a = parent[a]
+        b = parent[b]
+    return a
+
+dfs(1, 0) # 루트 노드는 1번 노드
+
+m = int(input())
+
+for i in range(m):
+    a ,b = map(int, input().split())
+    print(lca(a,b))
+# -
+
+# ## 개선된 최소 공통조상 
+
+# +
+# import sys
+# input = sys.stdin.readline # 시간초과 피하기 위한 빠른 입력 함수
+# sys.setrecursionlimit(int(1e5)) # 런타임 오류를 피하기
+LOG = 21 # 2^20 = 1,000,000  # 최대 백만개 들어갈수있다고 가정하고 짠 코드
+
+
+n = int(input())
+parent = [[0] * LOG for _ in range(n + 1)] # 부모 노드 정보
+d = [0] * (n + 1)  # 각 노드 까지의 깊이
+c = [0] * (n + 1)  # 각 노드의 깊이가 계산되었는지 여부
+graph = [[] for _ in range(n +1)] # 그래프 정보
+
+for _ in range(n -1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+    
+# 루트 노드부터 시작하여 깊이를 구하는 함수
+def dfs(x, depth):
+    c[x] = True
+    d[x] = depth
+    for y in graph[x]:
+        if c[y]: # 이미 깊이를 구했다면 넘기기
+            continue
+        parent[y][0] = x
+        dfs(y, depth +1)
+
+# 전체 부모관계를 설정하는 함수
+def set_parent():
+    dfs( 1, 0) # 루트 노드는 1번 노드
+    for i in range(1, LOG):
+        for j in range(1 , n+1):
+            parent[j][i] = parent[parent[j][i -1]][i-1]
+        
+        
+# A와 B의 최소 공통 조상을 찾는 함수
+def lca(a,b):
+    # b가 더 깊도록 설정 # 항상 이렇게 되도록 설정함 
+    if d[a] > d[b]:
+        a, b = b ,a
+    # 먼저 깊이가 동일하도록 
+    for i in range(LOG -1, -1, -1):
+        if d[b] - d[a] >= (1 << i):
+            b = parent[b][i]
+            
+    # 부모가 같아지도록
+    if a==b :
+        return a;
+    for i in range(LOG -1 , -1, -1):
+        # 조상을 향해 거슬러 올라가기
+        if parent[a][i] != parent[b][i]:
+            a= parent[a][i]
+            b = parent[b][i]
+            
+    # 이후에 부모가 찾고자 하는 조상
+    return parent[a][0]
+
+set_parent()
+
+m = int(input())
+
+for i in range(m):
+    a ,b = map(int, input().split())
+    print(lca(a,b))
