45126683b250b03dc9fd51769216849dcbe6868b
leehj01 <leehjung55@gmail.com>
2021-02-07 00:27:11 +0900

    탐욕법_체육복

--- programmers/210206_탐욕법_체육복.py
+++ programmers/210206_탐욕법_체육복.py
@@ -71,6 +71,22 @@ lost = [1,3,6,8]
 reserve = [2,4,7]
 
 solution(n, lost, reserve)
+
+
+# +
+# 같이 보면 좋을 코드
+
+def solution(n, lost, reserve):
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    for r in _reserve:
+        f = r - 1
+        b = r + 1
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    return n - len(_lost)  # 나는 체육복입을 학생의 수를 생각했으나, 여긴그냥 n에서 lost를 뻄
 # -
 
 
