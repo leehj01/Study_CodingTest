c5dc359c79de997a8b17be5eb7838364535d3a34
leehj01 <leehjung55@gmail.com>
2021-02-23 23:35:49 +0900

    해시_완주하지못한 선수

---
+++ programmers/210223_해시_완주하지못한 선수.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+#
+# 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+#
+# 제한사항
+# 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+# completion의 길이는 participant의 길이보다 1 작습니다.
+# 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+# 참가자 중에는 동명이인이 있을 수 있습니다.
+
+participant = ['mislav', 'stanko', 'mislav','ana']
+completion = ['stanko', 'ana','mislav']
+
+from collections import Counter
+def solution(participant, completion):
+    return ''.join(list((Counter(participant) - Counter(completion)).keys()))
