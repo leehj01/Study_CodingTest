94dd166cafcbab7994e61f84d2133b724be9af15
leehj01 <leehjung55@gmail.com>
2021-03-06 20:45:02 +0900

    탐욕법_큰수만들기

--- programmers/210225_탐욕법_큰수만들기.py
+++ programmers/210225_탐욕법_큰수만들기.py
@@ -14,18 +14,7 @@
 #     name: python3
 # ---
 
-# 문제 설명
-# 어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
-#
-# 예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
-#
-# 문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
-#
-# 제한 조건
-# number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
-# k는 1 이상 number의 자릿수 미만인 자연수입니다.
-
-# ## 처음 했던 방법
+# ## 처음 했던 방법 : 부분 리스트의 max 값 보다 작으면 없애기
 
 def solution(number, k):
     num = [i for i in number]
@@ -44,41 +33,64 @@ def solution(number, k):
     return ''.join(num)
 
 
-# ## 다시 시작
+solution(number, k)
 
-def pop_now( num ):
+# ## 세번째 방법 : 재귀함수 방법
+
+number = '1924'
+k = 2
+num = list(number)
+length = len(list(number)) - k
+
+
+# +
+def pop_now( num ):  # 뒤에보다 작은애는 없애는 함수 
     now = 0
     for i in range(len(num) -1 ):
-        if num[i] == '9':
-            break
-            
         if num[i] < num[i+1]:
             num.pop(now)
             break
         else :
             now += 1 
 
+def solution(number, k):
+    num = list(number)
+    length = len(list(number)) - k
+    while True :
 
-# +
-number = "99991"
-num = list(number)
-k = 3
-length = len(list(number)) - k
+        pop_now( num )
+        k -= 1
 
-while True :
-    
-    pop_now( num )
-    k -= 1
-    
-    if k == 0:
-        break
-        
-if len(num) != length  :
-    num = num[:length]
-    
-print(''.join(num))
+        if k == 0:
+            break
+
+    if len(num) != length  :
+        num = num[:length]
+             
+    return ''.join(num)
+
+
+
 # -
 
-num[:4]
+solution(number, k)
 
 
+# ## 마지막 방법 : 스택 방법
+
+def solution(number, k):
+    
+    stack = []
+    length = len(list(number)) - k
+
+    for i in number :
+        while stack and stack[-1] < i and k > 0 :
+            stack.pop()
+            k -= 1
+        stack.append(i)
+
+    if k != 0 :
+        stack = number[:length]
+
+             
+    return ''.join(stack)
