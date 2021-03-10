d91beb3d8eaed06fb9cc0d671640c4aa7879ddd8
leehj01 <leehjung55@gmail.com>
2021-03-10 23:47:57 +0900

    스택큐_주식가격

---
+++ programmers/210309_스택큐_주식가격.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# 문제 설명
+# 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+#
+# 제한사항
+# prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
+# prices의 길이는 2 이상 100,000 이하입니다.
+
+prices = [1, 2, 3, 2, 3]
+
+prices.pop()
+
+
