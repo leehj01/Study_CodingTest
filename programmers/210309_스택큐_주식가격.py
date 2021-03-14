685053d6a5d3c543b8b2d95eddcf3f40a0335f06
leehj01 <leehjung55@gmail.com>
2021-03-15 00:31:24 +0900

    스택큐_주식가격

--- programmers/210309_스택큐_주식가격.py
+++ programmers/210309_스택큐_주식가격.py
@@ -69,26 +69,38 @@ def solution(prices):
     return answer
 
 
-prices = [1, 2, 3, 4, 3]
+[1, 2, 3, 4, 3]
+
+prices = [5, 8, 6, 2, 4, 1] 
 
 # +
-prices = [1, 2, 3, 4, 3]
+prices =[1, 2, 3, 2, 3, 3]
 result =[0] * len(prices)
 
 for i, v in enumerate(prices):
-    cnt = 0
     for j, x in enumerate(prices[i:]):
-        print(v, 'x', x, 'i', i , )
-        if v <= x and i <= j :
+        if v <= x :
+            print()
             result[i] += 1
             
         else :
-            result[i] += 0 
-    
-print(result)
+            break
+            
+# answer = list(map(lambda n, m :  n-1  if m < prices[-1] else n , result, prices))
+# print(answer)
+# answer[-1] = 0
+# answer
 result
+
 # -
 
+
+prices[-1]
+
+r = list(map(lambda a,b: a+b, [1,2,3], [10,20,30]))
+print(r) # [11, 22, 33]
+
+list(map(lambda n, m :  n-1  if m <= prices[-1] else n , result, prices))
 
 # ## 세번째 시도
 
