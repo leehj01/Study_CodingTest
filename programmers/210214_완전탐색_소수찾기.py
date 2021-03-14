69ab29a2c76583c0dd90a79290adbdaa90f6e993
leehj01 <leehjung55@gmail.com>
2021-03-15 00:31:38 +0900

    완전탐색_소수찾기

--- programmers/210214_완전탐색_소수찾기.py
+++ programmers/210214_완전탐색_소수찾기.py
@@ -19,6 +19,9 @@
 #
 # 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
+from itertools import permutations
+
+
 # +
 # 내 풀이 
 def search(num):  # 소수를 판별해주는 코드. ( 이코테 마지막 강의에 수록된 것이 나와서 반갑 )
