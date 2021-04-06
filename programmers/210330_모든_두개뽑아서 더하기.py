3340518f487a8ff48be57ddacb85551123ec3674
leehj01 <leehjung55@gmail.com>
2021-04-06 22:59:23 +0900

    두개뽑아서 더하기

---
+++ programmers/210330_모든_두개뽑아서 더하기.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(list(set(answer)))
